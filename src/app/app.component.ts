cf20399fb7120539a879058cb392ac73b05c66b4
Dinh Xuan Ny <nydx@aureole.local>
2022-07-07 15:31:25 +0700

    sang10diem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'new-todo';
+  users: {name:string, title:string }[]=[
+    {name: 'Carla Espinosa', title:'Nurse'},
+    { name: 'Bob Kelso', title: 'Doctor of Medicine' },
+    { name: 'Janitor', title: 'Janitor' },
+    { name: 'Perry Cox', title: 'Doctor of Medicine' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+  ];
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
+  drop(event: CdkDragDrop<string[]>){
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NbLayoutModule, NbSidebarModule, NbButtonModule, NbThemeModule, NbCardModule, NbInputModule, NbIconModule, NbListModule, NbUserModule } from '@nebular/theme';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodosComponent } from './todos/todos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
+    NbLayoutModule,
+    NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+    NbThemeModule.forRoot(),
+    NbCardModule,
+    NbInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbListModule,
+    NbUserModule,
+    NbIconModule,
+    DragDropModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
